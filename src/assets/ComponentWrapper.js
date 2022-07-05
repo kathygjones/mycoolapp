8075dbc1882726d983a732b94cd337911d77020d
kathygjones <kumqwat@churchofjesuschrist.org>
2022-07-05 13:45:18 -0400

    begin styling on main profile component

--- src/assets/ComponentWrapper.js
+++ src/assets/ComponentWrapper.js
@@ -3,6 +3,7 @@ import { useParams } from '@fs/zion-router'
 import MyFamily from '../components/my_family/src/MyFamily'
 import DadJokes from '../components/dad_jokes/src/DadJokes'
 import MovieLoadingSkeleton from '../components/favorite_movies/MovieLoadingSkeleton'
+import VendingMachine from '../components/vending_machine/VendingMachine'
 
 const FavoriteMovies = React.lazy(() => import('../components/favorite_movies/FavoriteMoviesPage'))
 
@@ -20,7 +21,8 @@ export default function ComponentWrapper({ hexString }) {
           <FavoriteMovies randomColor={hexString} />
         </Suspense>
       )
-
+    case 'vending-machine':
+      return <VendingMachine />
     default:
       return <div>Whoopsie!</div>
   }
--- src/assets/MyProfile.js
+++ src/assets/MyProfile.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useRef } from 'react'
 import { Switch, Route, Link, useRouteMatch } from '@fs/zion-router'
-// import { css } from '@emotion/core'
+import { css } from '@emotion/core'
+import { Cell, Grid } from '@fs/zion-ui'
 import ComponentWrapper from './ComponentWrapper'
 
 export default function MyProfile() {
@@ -14,28 +15,35 @@ export default function MyProfile() {
   }, [hexString])
 
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to={`${url}/my-family`}>My Family</Link>
-        </li>
-        <li>
-          <Link to={`${url}/favorite-movies`} onClick={generateHexString}>
-            My Favorite Movies
-          </Link>
-        </li>
-        <li>
-          <Link to={`${url}/dad-jokes`}>Dad Jokes</Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route exact path={path}>
-          <h1>Please select something</h1>
-        </Route>
-        <Route path={`${path}/:selectionId`}>
-          <ComponentWrapper hexString={hexString.current} />
-        </Route>
-      </Switch>
-    </div>
+    <Grid>
+      <Cell columns={4}>
+        <ul>
+          <li>
+            <Link to={`${url}/my-family`}>My Family</Link>
+          </li>
+          <li>
+            <Link to={`${url}/favorite-movies`} onClick={generateHexString}>
+              My Favorite Movies
+            </Link>
+          </li>
+          <li>
+            <Link to={`${url}/dad-jokes`}>Dad Jokes</Link>
+          </li>
+          <li>
+            <Link to={`${url}/vending-machine`}>Vending machine</Link>
+          </li>
+        </ul>
+      </Cell>
+      <Cell columns={8}>
+        <Switch>
+          <Route exact path={path}>
+            <h1>Please select something</h1>
+          </Route>
+          <Route path={`${path}/:selectionId`}>
+            <ComponentWrapper hexString={hexString.current} />
+          </Route>
+        </Switch>
+      </Cell>
+    </Grid>
   )
 }
