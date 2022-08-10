2848f0f6366d6e821712cf263685e6bc17975efc
kathygjones <kumqwat@churchofjesuschrist.org>
2022-08-10 16:53:55 -0400

    add translations

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Switch, Route, Link, useRouteMatch } from '@fs/zion-router'
 import { css } from '@emotion/core'
-import { Cell, Grid } from '@fs/zion-ui'
+import { Cell, Grid, LayoutBand, Button } from '@fs/zion-ui'
+import { useTranslation, Trans } from 'react-i18next'
 import ComponentWrapper from './ComponentWrapper'
 
 const navLinksCss = css`
@@ -14,32 +15,33 @@ const navLinksCss = css`
 `
 export default function MyProfile() {
   const { path, url } = useRouteMatch()
+  const { t } = useTranslation()
 
   return (
     <Grid>
       <Cell columns={3}>
         <ul css={navLinksCss}>
           <li>
-            <Link to={`${url}`}>About Me</Link>
+            <Link to={`${url}`}>{t('about.me', 'About Me')}</Link>
           </li>
           <li>
-            <Link to={`${url}/my-family`}>My Family</Link>
+            <Link to={`${url}/my-family`}>{t('my.family', 'My Family')}</Link>
           </li>
           <li>
-            <Link to={`${url}/favorite-movies`}>My Favorite Movies</Link>
+            <Link to={`${url}/favorite-movies`}>{t('my.favorite.movies', 'My Favorite Movies')}</Link>
           </li>
           <li>
-            <Link to={`${url}/dad-jokes`}>Dad Jokes</Link>
+            <Link to={`${url}/dad-jokes`}>{t('dad.jokes', 'Dad Jokes')}</Link>
           </li>
           <li>
-            <Link to={`${url}/vending-machine`}>Vending machine</Link>
+            <Link to={`${url}/vending-machine`}>{t('vending.machine', 'Vending machine')}</Link>
           </li>
         </ul>
       </Cell>
       <Cell columns={9}>
         <Switch>
           <Route exact path={path}>
-            <h1>Please select something</h1>
+            <AboutMe />
           </Route>
           <Route path={`${path}/:selectionId`}>
             <ComponentWrapper />
@@ -47,5 +49,24 @@ export default function MyProfile() {
         </Switch>
       </Cell>
     </Grid>
+  )
+}
+
+function AboutMe() {
+  const link1 = 'https://kathy-jones.com/'
+  const link2 = 'https://github.com/katherinegjones'
+  return (
+    <LayoutBand>
+      <div>
+        <Trans
+          i18nKey="about.me.message"
+          defaults="Hello there! I am Kathy Jones, an avid musician, web developer, and mom. See my work on <link1>my personal webpage</link1> and <link2>my github repo</link2>."
+          components={{
+            link1: <Button emphasis="low" to={link1} external target="_blank" />,
+            link2: <Button emphasis="low" to={link2} external target="_blank" />,
+          }}
+        />
+      </div>
+    </LayoutBand>
   )
 }
